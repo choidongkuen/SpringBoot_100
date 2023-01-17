23aeaa97eb3051e0c9ba3d85e7acd450d4e9bb34
choidongkuen <danaver12@daum.net>
2023-01-18 03:47:34 +0900

    [#5] feat : UserDto from 정적 메소드 추가

--- src/main/java/com/example/springboot100/user/domain/dto/UserDto.java
+++ src/main/java/com/example/springboot100/user/domain/dto/UserDto.java
@@ -1,6 +1,7 @@
 package com.example.springboot100.user.domain.dto;
 
 
+import com.example.springboot100.user.domain.entity.User;
 import lombok.*;
 
 @Getter
@@ -15,4 +16,13 @@ public class UserDto {
     private String name;
 
     private String phone;
+
+    public static UserDto from(User user) {
+
+        return UserDto.builder()
+                .email(user.getEmail())
+                .name(user.getName())
+                .phone(user.getPhone())
+                .build();
+    }
 }
