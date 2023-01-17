a85d5642b7efe2cffc7997b3746e1c17439ff7b8
choidongkuen <danaver12@daum.net>
2023-01-18 01:49:08 +0900

    [#3] refactor : UserException 리팩토링

--- src/main/java/com/example/springboot100/user/exception/UserException.java
+++ src/main/java/com/example/springboot100/user/exception/UserException.java
@@ -1,8 +1,20 @@
 package com.example.springboot100.user.exception;
 
-import org.springframework.http.ResponseEntity;
+import com.example.springboot100.exception.ErrorCode;
 
-public class UserException extends RuntimeException{
-    public static ResponseEntity<UserExceptionResponse> toResponseEntity(UserException e) {
+public class UserException extends RuntimeException {
+
+    private ErrorCode errorCode;
+
+    public UserException(ErrorCode errorCode) {
+
+        this.errorCode = errorCode;
+
+    }
+
+    public UserException(ErrorCode errorCode, String message) {
+
+        super(message);
+        this.errorCode = errorCode;
     }
 }
