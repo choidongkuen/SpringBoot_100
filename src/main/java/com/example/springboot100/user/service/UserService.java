b5698738fb6f9cbf6632a448bbd0152201cc6c58
choidongkuen <danaver12@daum.net>
2023-01-18 01:58:27 +0900

    [#3] feat : UserService구현

--- src/main/java/com/example/springboot100/user/service/UserService.java
+++ src/main/java/com/example/springboot100/user/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.springboot100.user.service;
 
+import com.example.springboot100.user.domain.dto.UserUpdateDto.UserUpdateRequest;
+
 import static com.example.springboot100.user.domain.dto.UserCreateDto.UserCreateRequest;
 import static com.example.springboot100.user.domain.dto.UserCreateDto.UserCreateResponse;
 
@@ -7,4 +9,5 @@ public interface UserService {
 
     UserCreateResponse addUser(UserCreateRequest request);
 
+    Object updateUser(Long id, UserUpdateRequest request);
 }
