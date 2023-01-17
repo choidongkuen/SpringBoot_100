5c522600e40f9f457deec4f6bee7d3a1c912e071
choidongkuen <danaver12@daum.net>
2023-01-18 01:11:49 +0900

    [#1] feat : 회원등록 UserService 구현

--- src/main/java/com/example/springboot100/user/domain/repository/UserRepository.java
+++ src/main/java/com/example/springboot100/user/domain/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.springboot100.user.domain.repository;
 
 import com.example.springboot100.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
