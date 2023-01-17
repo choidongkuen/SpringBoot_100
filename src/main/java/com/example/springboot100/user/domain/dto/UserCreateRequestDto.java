64ec2d7c5cfb5154c8b9ecb9972fdd4890f1ac26
choidongkuen <danaver12@daum.net>
2023-01-17 23:43:32 +0900

    [#1] feat : UserCreateRequestDto 구현

---
+++ src/main/java/com/example/springboot100/user/domain/dto/UserCreateRequestDto.java
@@ -0,0 +1,33 @@
+package com.example.springboot100.user.domain.dto;
+
+
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserCreateRequestDto {
+
+    @Email(message = "이메일 형식에 맞게 입력해 주세요.")
+    @NotBlank(message = "이메일은 필수 항목 입니다.")
+    private String email;
+
+    @NotBlank(message = "이름은 필수 항목 입니다.")
+    private String name;
+
+    @NotBlank(message = "비밀번호는 필수 항목 입니다.")
+    @Size(min = 4, message = "비밀번호는 4자 이상 입력해야 합니다.")
+    private String password;
+
+    @NotBlank(message = "전화번호는 필수 항목 입니다.")
+    @Size(message = "연락처는 필수 항목 입니다.")
+    private String phone;
+
+}
