ea2cb3dae35364b548cf44c961491537702fd4b2
choidongkuen <danaver12@daum.net>
2023-01-17 23:52:52 +0900

    [#1] feat : ApiUserController 회원 등록 컨트롤러 구현

---
+++ src/main/java/com/example/springboot100/user/controller/ApiUserController.java
@@ -0,0 +1,52 @@
+package com.example.springboot100.user.controller;
+
+import com.example.springboot100.notice.exception.ResponseError;
+import com.example.springboot100.user.domain.dto.UserCreateRequestDto;
+import com.example.springboot100.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class ApiUserController {
+
+    private final UserService userService;
+
+    @PostMapping("/api/user")
+    public ResponseEntity<Object> addUser(
+            @RequestBody @Valid UserCreateRequestDto request,
+            Errors errors)
+    {
+        if(errors.hasErrors()) {
+
+            List<ResponseError> responseErrors = new ArrayList<>();
+
+            errors.getAllErrors().forEach(e -> {
+
+                responseErrors.add(ResponseError.of((FieldError) e));
+
+            });
+
+            return new ResponseEntity<>(
+                    responseErrors, HttpStatus.BAD_REQUEST
+            );
+
+        }
+
+        return new ResponseEntity<>(
+                userService.addUser(request),HttpStatus.OK
+        );
+    }
+}
