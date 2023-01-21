626cdbc818e0f2cf8d06a11acbd3729489fb89a2
choidongkuen <danaver12@daum.net>
2023-01-21 21:46:20 +0900

    [#21] feat : ErrorCode 추가

--- src/main/java/com/example/springboot100/exception/ErrorCode.java
+++ src/main/java/com/example/springboot100/exception/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
 
     NO_FOUND_NOTICE("해당하는 게시글 정보가 없습니다.", HttpStatus.BAD_REQUEST),
 
-    NO_FOUND_USER("해당하는 사용자 정보가 없습니다.", HttpStatus.BAD_REQUEST);
+    NO_FOUND_USER("해당하는 사용자 정보가 없습니다.", HttpStatus.BAD_REQUEST),
+    NO_MATCH_PASSWORD("비밀번호가 일치하지 않습니다", HttpStatus.BAD_REQUEST);
 
     private final String message;
     private final HttpStatus httpStatus;
