c438949194502388599ae9cb1ce911892d7374bb
choidongkuen <danaver12@daum.net>
2023-01-22 03:19:17 +0900

    [#25] feat : 모든 회원 정보 및 회원 수 조회 컨트롤러

---
+++ src/main/java/com/example/springboot100/admin/controller/ApiAdminController.java
@@ -0,0 +1,26 @@
+package com.example.springboot100.admin.controller;
+
+import com.example.springboot100.admin.domain.dto.UserInfoResponseDto;
+import com.example.springboot100.admin.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class ApiAdminController {
+    private final AdminService adminService;
+
+    @GetMapping("/api/admin/user")
+    public ResponseEntity<UserInfoResponseDto> getUserInfo(
+
+    ) {
+        return new ResponseEntity<>(
+                adminService.getUserInfo(), HttpStatus.OK
+        );
+    }
+}
