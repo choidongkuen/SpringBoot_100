37c63dfa8c9c0d14dee25e7f160e400f214108c9
choidongkuen <danaver12@daum.net>
2023-01-25 03:34:19 +0900

    feat : 좋아요 내림차순 조회 finduserLikeBest 메소드 구현

--- src/main/java/com/example/springboot100/user/domain/repository/UserCustomRepository.java
+++ src/main/java/com/example/springboot100/user/domain/repository/UserCustomRepository.java
@@ -28,10 +28,22 @@ public class UserCustomRepository {
     public List<UserLogDto> findUserLogCount() {
 
         String sql
+
                 = " select u.id, u.email, u.name," +
                 " (select count(*) from notice n where n.user_id = u.id) notice_count, " +
                 " (select count(*) from notice_like nl where nl.user_id = u.id) notice_like_count " +
                 " from user_table u ";
+
+        return entityManager.createNativeQuery(sql).getResultList();
+
+    }
+
+    public List<UserLogDto> findUserLikeBest() {
+
+        String sql = " select t1.id, t1.email, t1.name, t1.notice_like_count "
+                + " from ( select u.*, (select count(*) from notice_like nl "
+                + " where nl.user_id = u.id ) as notice_like_count from user_table u )t1"
+                + " order by t1.notice_like_count desc";
 
         return entityManager.createNativeQuery(sql).getResultList();
 
