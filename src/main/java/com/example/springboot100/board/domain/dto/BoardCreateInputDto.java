110ccddcd3ada1b1d8d2988c8fa6cfefdac49003
choidongkuen <danaver12@daum.net>
2023-01-25 05:24:09 +0900

    [#33] refactor : BoardCreateInputDto validation 수정

--- src/main/java/com/example/springboot100/board/domain/dto/BoardCreateInputDto.java
+++ src/main/java/com/example/springboot100/board/domain/dto/BoardCreateInputDto.java
@@ -15,7 +15,7 @@ import javax.validation.constraints.Size;
 public class BoardCreateInputDto {
 
     @NotBlank(message = "게시판 제목은 필수 입력값입니다.")
-    @Size(min = 1, max = 50, message = "게시판 제목은 최대 50자 입니다.")
+    @Size(max = 50, message = "게시판 제목은 최대 50자 입니다.")
     private String boardName;
 
     @NotEmpty(message = "게시판 내용은 필수 입력값입니다.")
