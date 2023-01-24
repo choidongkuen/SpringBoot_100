024101b794d9daf0df69152a58d80cd94601171f
choidongkuen <danaver12@daum.net>
2023-01-24 19:26:20 +0900

    feat : UserLogDto 구현
     사용자별 게시글 수와 좋아요 수 관련 응답dto

---
+++ src/main/java/com/example/springboot100/user/domain/dto/UserLogDto.java
@@ -0,0 +1,22 @@
+package com.example.springboot100.user.domain.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLogDto {
+
+    private Long userId;
+
+    private String email;
+
+    private String userName;
+
+    private Long noticeCount;
+
+    private Long userLikeCount;
+}
