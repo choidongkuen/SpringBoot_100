96fda3f1c9c209ff05cbe23240c27b965ad5be19
choidongkuen <danaver12@daum.net>
2023-01-25 04:56:27 +0900

    [#33] feat : 게시판 등록 요청 dto 구현

---
+++ src/main/java/com/example/springboot100/board/domain/dto/BoardCreateInputDto.java
@@ -0,0 +1,25 @@
+package com.example.springboot100.board.domain.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardCreateInputDto {
+
+    @NotBlank(message = "게시판 제목은 필수 입력값입니다.")
+    @Size(min = 1, max = 50, message = "게시판 제목은 최대 50자 입니다.")
+    private String boardName;
+
+    @NotEmpty(message = "게시판 내용은 필수 입력값입니다.")
+    @Size(max = 500, message = "게시판 내용은 최대 500자 입니다.")
+    private String boardContent;
+
+}
