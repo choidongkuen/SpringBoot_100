55ce1f17064e95e16e701525c4d6c1b8caff965d
choidongkuen <danaver12@daum.net>
2023-01-27 00:55:05 +0900

    [#35] refactor : BoardTypeRepository 수정

--- src/main/java/com/example/springboot100/board/domain/repository/BoardTypeRepository.java
+++ src/main/java/com/example/springboot100/board/domain/repository/BoardTypeRepository.java
@@ -1,11 +1,11 @@
 package com.example.springboot100.board.domain.repository;
 
-import com.example.springboot100.board.domain.entity.Board;
+import com.example.springboot100.board.domain.entity.BoardType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface BoardTypeRepository extends JpaRepository<Board, Long> {
+public interface BoardTypeRepository extends JpaRepository<BoardType, Long> {
 
-    Optional<Board> findByName(String name);
+    Optional<BoardType> findByName(String name);
 }
